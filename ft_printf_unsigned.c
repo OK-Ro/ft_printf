4ebefd20aadc659b0944955fb046c6670b50bf29
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-02 19:14:16 +0200

    u[date

--- ft_printf_unsigned.c
+++ ft_printf_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 12:24:47 by rokuni            #+#    #+#             */
-/*   Updated: 2026/03/31 14:00:28 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/02 19:12:27 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_printf_unsigned(unsigned int n)
 
 	count = 0;
 	nb = n;
-	if (nb > 10)
+	if (nb >= 10)
 		count += ft_printf_unsigned(nb / 10);
 	count += ft_printf_char(nb % 10 + '0');
 	return (count);
